81888c8b4c8167926fc4f51c1a15d271440595e8
Souvik <souvik3469@gmail.com>
2023-02-22 00:30:49 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles/app.scss"
+
+function App() {
+  return (
+    <div className="App">
+      App
+    </div>
+  );
+}
+
+export default App;
