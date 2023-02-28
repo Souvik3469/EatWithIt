24db5f9c2d418a154242c49b073e934a2e74ef4e
Souvik <souvik3469@gmail.com>
2023-03-01 00:11:41 +0530

    home header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles/app.scss"
+import "./styles/header.scss";
+import "./styles/home.scss";
+import Home from "./components/home/Home";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/"element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
